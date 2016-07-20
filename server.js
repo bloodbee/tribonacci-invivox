2f4c0797bc7618205fd39bed864dbf3fb44cfb76
Mathieu Dufour <mathieu_dufour@hotmail.fr>
2016-07-20 18:23:36 +0200

    assertions and siplayed results

--- server.js
+++ server.js
@@ -34,7 +34,12 @@ function tribonacci(signature, iterations) {
         return tribonacci_sequence;
     }
 
-    server.close(); // the signature is not 3 numbers length, the tribonacci sequence cannot be done.
+    assert.throws(
+      () => {
+        throw new Error('The signature must be 3 numbers legnth');
+      },
+      Error
+    ); // the signature is not 3 numbers length, the tribonacci sequence cannot be done.
 };
 
 
@@ -44,37 +49,35 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
 
     // let's start our tests
-    res.write('tribonacci([1,1,1],10) = [1,1,1,3,5,9,17,31,57,105] ? ' +
-        assert.deepEqual(tribonacci([1,1,1],10), [1,1,1,3,5,9,17,31,57,105]) + '\n'
-    );
-    res.write('tribonacci([0,0,1],10) = [0,0,1,1,2,4,7,13,24,44] ? ' +
-        assert.deepEqual(tribonacci([0,0,1],10), [0,0,1,1,2,4,7,13,24,44]) + '\n'
-    );
-    res.write('tribonacci([0,1,1],10) = [0,1,1,2,4,7,13,24,44,81] ? ' +
-        assert.deepEqual(tribonacci([0,1,1],10), [0,1,1,2,4,7,13,24,44,81]) + '\n'
-    );
-    res.write('tribonacci([1,0,0],10) = [1,0,0,1,1,2,4,7,13,24] ? ' +
-        assert.deepEqual(tribonacci([1,0,0],10), [1,0,0,1,1,2,4,7,13,24]) + '\n'
-    );
-    res.write('tribonacci([0,0,0],10) = [0,0,0,0,0,0,0,0,0,0] ? ' +
-        assert.deepEqual(tribonacci([0,0,0],10), [0,0,0,0,0,0,0,0,0,0]) + '\n'
-    );
-    res.write('tribonacci([1,2,3],10) = [1,2,3,6,11,20,37,68,125,230] ? ' +
-        assert.deepEqual(tribonacci([1,2,3],10), [1,2,3,6,11,20,37,68,125,230]) + '\n'
-    );
-    res.write('tribonacci([3,2,1],10) = [3,2,1,6,9,16,31,56,103,190] ? ' +
-        assert.deepEqual(tribonacci([3,2,1],10), [3,2,1,6,9,16,31,56,103,190]) + '\n'
-    );
-    res.write('tribonacci([1,1,1],1) = [1] ? ' +
-        assert.deepEqual(tribonacci([1,1,1],1), [1]) + '\n'
-    );
-    res.write('tribonacci([300,200,100],0) = []) ? ' +
-        assert.deepEqual(tribonacci([300,200,100],0), []) + '\n'
-    );
-    res.write('tribonacci([0.5,0.5,0.5],30) = [0.5,0.5,0.5,1.5,2.5,4.5,8.5,15.5,28.5,52.5,96.5,177.5,326.5,600.5,1104.5,2031.5,3736.5,6872.5,12640.5,23249.5,42762.5,78652.5,144664.5,266079.5,489396.5,900140.5,1655616.5,3045153.5,5600910.5,10301680.5]) ? ' +
-        assert.deepEqual(tribonacci([0.5,0.5,0.5],30), [0.5,0.5,0.5,1.5,2.5,4.5,8.5,15.5,28.5,52.5,96.5,177.5,326.5,600.5,1104.5,2031.5,3736.5,6872.5,12640.5,23249.5,42762.5,78652.5,144664.5,266079.5,489396.5,900140.5,1655616.5,3045153.5,5600910.5,10301680.5]) + '\n'
-    );
+    assert.deepEqual(tribonacci([1,1,1],10), [1,1,1,3,5,9,17,31,57,105]); // make the assertion
+    res.write('tribonacci([1,1,1],10) = [1,1,1,3,5,9,17,31,57,105] ? True\n'); // if the assert is true, the response can be displayed, otherwise it can't
 
+    assert.deepEqual(tribonacci([0,0,1],10), [0,0,1,1,2,4,7,13,24,44]);
+    res.write('tribonacci([0,0,1],10) = [0,0,1,1,2,4,7,13,24,44] ? True\n');
+
+    assert.deepEqual(tribonacci([0,1,1],10), [0,1,1,2,4,7,13,24,44,81]);
+    res.write('tribonacci([0,1,1],10) = [0,1,1,2,4,7,13,24,44,81] ? True\n');
+
+    assert.deepEqual(tribonacci([1,0,0],10), [1,0,0,1,1,2,4,7,13,24]);
+    res.write('tribonacci([1,0,0],10) = [1,0,0,1,1,2,4,7,13,24] ? True\n');
+
+    assert.deepEqual(tribonacci([0,0,0],10), [0,0,0,0,0,0,0,0,0,0]);
+    res.write('tribonacci([0,0,0],10) = [0,0,0,0,0,0,0,0,0,0] ? True\n');
+
+    assert.deepEqual(tribonacci([1,2,3],10), [1,2,3,6,11,20,37,68,125,230]);
+    res.write('tribonacci([1,2,3],10) = [1,2,3,6,11,20,37,68,125,230] ? True\n');
+
+    assert.deepEqual(tribonacci([3,2,1],10), [3,2,1,6,9,16,31,56,103,190]);
+    res.write('tribonacci([3,2,1],10) = [3,2,1,6,9,16,31,56,103,190] ? True\n');
+
+    assert.deepEqual(tribonacci([1,1,1],1), [1]);
+    res.write('tribonacci([1,1,1],1) = [1] ? True\n');
+
+    assert.deepEqual(tribonacci([300,200,100],0), []);
+    res.write('tribonacci([300,200,100],0) = []) ? True\n');
+
+    assert.deepEqual(tribonacci([0.5,0.5,0.5],30), [0.5,0.5,0.5,1.5,2.5,4.5,8.5,15.5,28.5,52.5,96.5,177.5,326.5,600.5,1104.5,2031.5,3736.5,6872.5,12640.5,23249.5,42762.5,78652.5,144664.5,266079.5,489396.5,900140.5,1655616.5,3045153.5,5600910.5,10301680.5]);
+    res.write('tribonacci([0.5,0.5,0.5],30) = [0.5,0.5,0.5,1.5,2.5,4.5,8.5,15.5,28.5,52.5,96.5,177.5,326.5,600.5,1104.5,2031.5,3736.5,6872.5,12640.5,23249.5,42762.5,78652.5,144664.5,266079.5,489396.5,900140.5,1655616.5,3045153.5,5600910.5,10301680.5]) ? True\n');
 
     // the res must end
     res.end('\n');
